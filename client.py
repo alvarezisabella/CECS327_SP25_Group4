4403c9c96358b634bf813ec79e1425993509ad26
alvarezisabella <isabellaalvarez003@gmail.com>
2025-05-05 21:51:14 -0700

    finalize client, attempt server queries

--- client.py
+++ client.py
@@ -1,30 +1,32 @@
-#client.py
 import socket
 
-serverIP = input("Enter server IP Address: ")
-serverPort = int(input("Enter server port number: "))
-maxBytesToReceive = 1024
+
+server_ip = input("Enter the server IP address: ")
+server_port = int(input("Enter the server port: "))
+
 myTCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-myTCPSocket.connect((serverIP, serverPort))
-validQuery = False
+myTCPSocket.connect((server_ip, server_port))
+
 
 while True:
-    myMessage = input("Please choose one of the following queries to process\n"
-                         "1. What is the average moisture inside my kitchen fridge in the past three hours?\n"
-                             "2. What is the average water consumption per cycle in my smart dishwasher?\n"
-                             "3. Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?\n")
-    if myMessage == "exit":
-        break
-    selection = int(myMessage)
-    if 1 <= selection <= 3:
-        validQuery = True
-    else:
-        print("Sorry, this query cannot be processes. Please try one of the following:\n"
-                         "1. What is the average moisture inside my kitchen fridge in the past three hours?\n"
-                             "2. What is the average water consumption per cycle in my smart dishwasher?\n"
-                             "3. Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?\n")
+    print("\nChoose a query:")
+    print("1) What is the average moisture inside my kitchen fridge in the past 3 hours?")
+    print("2) What is the average water consumption per cycle in my smart dishwasher?")
+    print("3) Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?)")
+    print("Type 'quit' to exit.")
 
-    myTCPSocket.send(bytearray(str(myMessage), encoding='utf-8'))
-    serverResponse = myTCPSocket.recv(maxBytesToReceive).decode('utf-8')
-    print("Server Response: ", serverResponse)
+    user_input = input("Enter your choice (1, 2, 3 or 'quit'): ")
+
+    if user_input in ["1", "2", "3", "quit"]:
+        myTCPSocket.send(user_input.encode('utf-8'))
+
+        if user_input == "quit":
+            break
+
+        response = myTCPSocket.recv(1024).decode('utf-8')
+        print("Server says:", response)
+    else:
+        print("Invalid choice. Please type 1, 2, 3, or 'quit'.")
+
 myTCPSocket.close()
+print("Disconnected from server.")
--- server.py
+++ server.py
@@ -1,26 +1,3 @@
-# # server.py
-# import socket
-#
-# serverIP = input("Enter server IP Address: ")
-# serverPort = int(input("Enter server port number: "))
-#
-# myTCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# myTCPSocket.bind((serverIP, serverPort))
-# myTCPSocket.listen(5)
-# incomingSocket, incomingAddress = myTCPSocket.accept()
-# maxBytesToReceive = 1024
-#
-# while True:
-#     myData = incomingSocket.recv(maxBytesToReceive).decode('utf-8')
-#     if not myData:
-#         break
-#     print("Client says: ", myData)
-#     myMessage = input("Enter a message to send to client: ")
-#     incomingSocket.send(bytearray(str(myMessage), encoding="utf-8"))
-#
-# incomingSocket.close()
-# myTCPSocket.close()
-
 import socket
 import psycopg2
 
@@ -34,7 +11,7 @@ incomingSocket, incomingAddress = myTCPSocket.accept()
 
 try:
     db_connection = psycopg2.connect(
-        "postgresql://neondb_owner:npg_ognWcMRFyS52@ep-withered-fog-a444emeu-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require")
+        "postgresql://neondb_owner:npg_F0ItnOsQ4kLv@ep-calm-moon-a5o0nb9m-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require")
     cursor = db_connection.cursor()
     print("Successfully connected to PostgreSQL")
     cursor.execute("SELECT version();")
@@ -50,16 +27,25 @@ try:
         print("Client says: ", clientData)
 
         if clientData == "1":
-            cursor.execute('SELECT AVG("value")'
-                           'FROM "Data_metadata" '
-                           'WHERE "assetUid" AND "assetType" = "Smart Refrigerator" ')
+            cursor.execute(
+                'SELECT payload ->> \'Moisture Meter - Refrigerator_Moisture\' AS moisture_value FROM "Data_virtual" WHERE payload ->> \'asset_uid\' = \'510-8u0-wpw-f29\';')  # INSERT QUERY 1 HERE
             data = cursor.fetchall()
             incomingSocket.send(bytearray(str(data), encoding='utf-8'))
         elif clientData == "2":
-            cursor.execute("SELECT ")  # INSERT QUERY 2 HERE
+            cursor.execute(
+                'SELECT AVG(CAST(payload ->> \'YF-S201 - Dishwasher_Water\' AS FLOAT)) AS avg_wtr_lit FROM "Data_virtual" WHERE payload ->> \'asses_uid\' = \'fci-ra7-433-615\';') # INSERT QUERY 2 HERE
         elif clientData == "3":
-            print("QUERY 3")  # INSERT QUERY # HERE
-
+            cursor.execute(
+            'SELECT device_id, SUM(kwh) AS total_kwh FROM (''SELECT \'510-8u0-wpw-f29\' AS device_id, payload ->> \'ACS712 - Refrigerator_Ammeter\' FROM "Data_virtual" WHERE payload ->> \'asset_uid\' = \'510-8u0-wpw-f29\' AND payload ->> \'ACS712 - Refrigerator_Ammeter\' IS NOT NULL'
+                'UNION ALL'
+                'SELECT \'4e308b46-9041-400c-a5ae-279f6ca506a9\' AS device_id, payload ->> \'sensor 3 35c551e7-b6ab-4f5c-9853-7ce4f6b58bbc\' FROM "Data_virtual" WHERE payload ->> \'asset_uid\' = \'4e308b46-9041-400c-a5ae-279f6ca506a9\' AND payload ->> \'sensor 3 35c551e7-b6ab-4f5c-9853-7ce4f6b58bbc\' IS NOT NULL'
+                'UNION ALL'
+                'SELECT \'fci-ra7-433-615\' AS device_id, payload ->> \'ACS712 - Dishwasher_Ammeter\' FROM "Data_virtual" WHERE payload ->> \'asset_uid\' = \'fci-ra7-433-615\' AND payload ->> \'ACS712 - Dishwasher_Ammeter\' IS NOT NULL'
+            ') AS all_data'
+            'GROUP BY device_id'
+            'ORDER BY total_kwh DESC'
+            'LIMIT 1;'
+            )  # INSERT QUERY #3 HERE
 
 except Exception as e:
     print("Error connecting to the database:", e)
